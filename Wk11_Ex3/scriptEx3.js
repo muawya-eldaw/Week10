f24c94e6a14ce2853618d6a0854c3808df4a2802
muwaya <muawya.eldaw@northampton.ac.uk>
2021-12-16 17:23:05 +0000

    Commiting Wk11  - exercise 3 solution

---
+++ Wk11_Ex3/scriptEx3.js
@@ -0,0 +1,20 @@
+// Important note: function only run if the have been called
+
+//Step 3
+// below is a function definition which is ignored by the js interpreter.
+// a function is executed only when it has been called.
+function myClickEvent() {
+	var element = document.getElementById('circle');
+	element.style.backgroundColor = 'blue';
+}
+
+//Step 2
+function myLoadFunction() {
+	var element = document.getElementById('circle');
+	element.style.opacity = '0.5';
+	element.addEventListener('click', myClickEvent); // here is a call for the function myClickEvent()
+}
+
+//Step 1
+document.addEventListener('DOMContentLoaded', myLoadFunction); // here is a call for the function myLoadFunction()
+
