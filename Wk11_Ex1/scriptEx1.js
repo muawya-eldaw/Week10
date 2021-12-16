9c6129db71278491ddb03afda7208bea03e6d4b4
muwaya <muawya.eldaw@northampton.ac.uk>
2021-12-16 17:01:17 +0000

    Commiting Wk11  - exercise 1 solution

---
+++ Wk11_Ex1/scriptEx1.js
@@ -0,0 +1,19 @@
+// Important note: function only run if the have been called
+
+//Step 3
+// below is a function definition which is ignored by the js interpreter.
+// a function is executed only when it has been called.
+function myClickEvent() {
+	alert('The button was pressed');
+}
+
+//Step 2
+function myLoadFunction() {
+	var element = document.getElementById('circle');
+	element.addEventListener('click', myClickEvent); // here is a call for the function myClickEvent()
+}
+
+//Step 1
+document.addEventListener('DOMContentLoaded', myLoadFunction); // here is a call for the function myLoadFunction()
+}
+
